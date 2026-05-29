5355e32743368d9e3c553d702fe8cbc8b0fdbeeb
redu-leul <leulrediet@gmail.com>
2026-05-29 21:14:10 +0300

    Fixing some things

--- AppFrame.java
+++ AppFrame.java
@@ -1,19 +1,24 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
+
 import javax.swing.border.Border;
 import javax.swing.*;
 import java.awt.*;
 
 public class AppFrame extends JFrame implements ActionListener {
-    JFrame frame = new JFrame();
+
     JButton Button = new JButton();
+
+    private Stopwatch stopwatch;
+    private Flashcard flashcard;
+
     public AppFrame() {
 
-        ImageIcon icon = new ImageIcon("images.png" );
+        ImageIcon icon = new ImageIcon("images.png");
         Border border = BorderFactory.createLineBorder(Color.DARK_GRAY);
 
         JLabel label = new JLabel("WELCOME TO THE STUDY APP");
+
         label.setIcon(icon);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.TOP);
@@ -24,43 +29,58 @@ public class AppFrame extends JFrame implements ActionListener {
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setBounds(450,100,350,300);
 
-
         JButton flashbutton = new JButton("Flashcards");
+
         flashbutton.setBounds(450,450,350,40);
         flashbutton.setFont(new Font("MV Boli", Font.BOLD,20));
-        flashbutton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY) );
+        flashbutton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
         flashbutton.setFocusPainted(false);
 
+        flashbutton.addActionListener(e -> {
+
+            flashcard = new Flashcard("", "");
+
+            flashcard.frame.setVisible(true);
+            flashcard.frame.toFront();
+        });
 
         JButton timerbutton = new JButton("Study Timer");
+
         timerbutton.setBounds(450,500,350,40);
         timerbutton.setFont(new Font("MV Boli", Font.BOLD,20));
-        timerbutton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY) );
+        timerbutton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
         timerbutton.setFocusPainted(false);
 
-        JFrame frame = new JFrame();
+        timerbutton.addActionListener(e -> {
+
+            stopwatch = new Stopwatch();
+
+            stopwatch.frame.setVisible(true);
+            stopwatch.frame.toFront();
+        });
+
         this.setTitle("MINI STUDY APP");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 600);
-        this.setVisible(true);
-        this.add(label);
+        this.setSize(1300,800);
         this.setLayout(null);
+
+        this.add(label);
         this.add(flashbutton);
         this.add(timerbutton);
 
         ImageIcon image = new ImageIcon("book.jpg");
+
         this.setIconImage(image.getImage());
         this.getContentPane().setBackground(new Color(115, 195, 249));
 
+        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == Button){
-            Button button = (Button) e.getSource();
+
+        if (e.getSource() == Button) {
 
         }
     }
 }
-
-
--- Flashcard.java
+++ Flashcard.java
@@ -1,53 +1,68 @@
 import java.awt.*;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public  class Flashcard extends StudyTool {
+public class Flashcard extends StudyTool implements ActionListener {
 
-    private String front;
-    private String back;
+    private String question;
+    private String answer;
 
-    JFrame frame = new JFrame();
-    JLabel label = new JLabel("You can start with your flashcards");
+    JFrame frame;
 
-    public Flashcard(String front, String back) {
+    JButton addButton;
+    JButton readButton;
+
+    public Flashcard(String question, String answer) {
+
         super("Flashcard");
-        this.front = front;
-        this.back = back;
 
-        label.setBounds(0,0,100,50);
-        label.setFont(new Font("MV Boli",Font.BOLD,25));
+        this.question = question;
+        this.answer = answer;
 
-        frame.add(label);
+        frame = new JFrame();
+
+        addButton = new JButton("ADD NEW CARD");
+        readButton = new JButton("READ CARD");
+
+        addButton.setBounds(100,200,200,50);
+        addButton.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        addButton.setFocusable(false);
+        addButton.addActionListener(this);
+
+        readButton.setBounds(100,300,200,50);
+        readButton.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        readButton.setFocusable(false);
+        readButton.addActionListener(this);
+
+        frame.add(addButton);
+        frame.add(readButton);
+
         frame.setTitle("Flashcards");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(600,600);
         frame.setLayout(null);
         frame.setVisible(true);
-
     }
 
-    public String getFront(){
-        return front;
+    public String getQuestion() {
+        return question;
     }
-    public String getBack(){
-        return back;
+
+    public String getAnswer() {
+        return answer;
     }
-    public void setFront(String front){
-        this.front = front;
+
+    public void setQuestion(String question) {
+        this.question = question;
     }
-    public void setBack(String back){
-        this.back = back;
-    }
-    public String getback(){
-        return back;
-    }
-    public void setback(String back){
-        this.back = back;
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
     }
 
     @Override
-    public void starttool(){
+    public void starttool() {
         IO.println("Flashcard Tool");
     }
 
@@ -57,8 +72,77 @@ public  class Flashcard extends StudyTool {
     }
 
     @Override
-    public void dispalyinfo(){
+    public void dispalyinfo() {
         IO.println("Flashcard Tool Started");
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == addButton) {
+
+            JFrame addFrame = new JFrame("Add New Card");
+
+            JLabel questionLabel = new JLabel("Enter Question:");
+            JLabel answerLabel = new JLabel("Enter Answer:");
+
+            JTextField questionField = new JTextField();
+            JTextField answerField = new JTextField();
+
+            JButton saveButton = new JButton("SAVE");
+
+            questionLabel.setBounds(50,50,150,30);
+            answerLabel.setBounds(50,150,150,30);
+
+            questionField.setBounds(200,50,200,30);
+            answerField.setBounds(200,150,200,30);
+
+            saveButton.setBounds(150,250,120,40);
+
+            addFrame.add(questionLabel);
+            addFrame.add(answerLabel);
+            addFrame.add(questionField);
+            addFrame.add(answerField);
+            addFrame.add(saveButton);
+
+            addFrame.setSize(500,400);
+            addFrame.setLayout(null);
+            addFrame.setVisible(true);
+
+            saveButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    question = questionField.getText();
+                    answer = answerField.getText();
+
+                    JOptionPane.showMessageDialog(null,"Card Saved!");
+
+                    addFrame.dispose();
+                }
+            });
+        }
+
+        if(e.getSource() == readButton) {
+
+            JFrame readFrame = new JFrame("Read Card");
+
+            JLabel questionText = new JLabel("Question: " + question);
+            JLabel answerText = new JLabel("Answer: " + answer);
+
+            questionText.setBounds(50,100,400,40);
+            answerText.setBounds(50,200,400,40);
+
+            questionText.setFont(new Font("MV Boli", Font.BOLD, 25));
+            answerText.setFont(new Font("MV Boli", Font.BOLD, 25));
+
+            readFrame.add(questionText);
+            readFrame.add(answerText);
+
+            readFrame.setSize(500,400);
+            readFrame.setLayout(null);
+            readFrame.setVisible(true);
+        }
+    }
 }
